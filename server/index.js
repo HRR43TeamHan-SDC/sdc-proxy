c0c8219ac8a6e59921dee263e500f930537a1db6
WarrenMfg <warrenmfg@icloud.com>
2020-02-26 17:48:02 -0500

    trying redirects

--- server/index.js
+++ server/index.js
@@ -11,24 +11,26 @@ app.use(express.json());
 
 // PHOTO GALLERY
 app.get('/api/photos/:id', (req, res) => {
-  axios.get(`http://ec2-3-133-85-12.us-east-2.compute.amazonaws.com:3009/api/photos/${req.params.id}`)
-    .then((response) => {
-      const stream = new Readable({
-        read() {
-          this.push(JSON.stringify(response.data));
-          this.push(null);
-        },
-      });
-      stream.pipe(res);
+  res.redirect(`http://ec2-3-133-85-12.us-east-2.compute.amazonaws.com:3009/api/photos/${req.params.id}`);
+  // axios.get(`http://ec2-3-133-85-12.us-east-2.compute.amazonaws.com:3009/api/photos/${req.params.id}`)
+  //   .then((response) => {
+  //     const stream = new Readable({
+  //       read() {
+  //         this.push(JSON.stringify(response.data));
+  //         this.push(null);
+  //       },
+  //     });
+  //     stream.pipe(res);
       // res.send(response.data);
-    })
+    // })
     .catch((err) => res.send(err));
 });
 
 app.post('/api/photos', (req, res) => {
-  axios.post(`http://ec2-3-133-85-12.us-east-2.compute.amazonaws.com:3009/api/photos`, req.body)
-    .then((response) => res.send(response))
-    .catch((err) => res.send(err));
+  res.redirect(`http://ec2-3-133-85-12.us-east-2.compute.amazonaws.com:3009/api/photos`);
+  // axios.post(`http://ec2-3-133-85-12.us-east-2.compute.amazonaws.com:3009/api/photos`, req.body)
+  //   .then((response) => res.send(response))
+  //   .catch((err) => res.send(err));
 });
 
 
